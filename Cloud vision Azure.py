8e8f96f3da0fe639c92827750a689daa3f0a4cc4
Rodrigo R <a01183284@tec.mx>
2023-04-01 00:44:20 -0600

    subiendo varioas pruebas

--- Cloud vision Azure.py
+++ Cloud vision Azure.py
@@ -6,7 +6,7 @@ subscription_key="16173823f64b4be0a1455fd78686a6dc"
 face_api_url = "https://facerecognitionmna23rodrigo.cognitiveservices.azure.com/"+"face/v1.0/detect"
 
 #este es el url de la imagen que vamos a analizar
-image_url = "" \
+image_url = "https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2019/11/hipertextual-100-000-caras-que-realidad-no-existen-2019554542.jpg?w=1500&quality=50&strip=all&ssl=1"
 
 #este es la forma en la que se envia la peticion a la api con el acceso y la imagen
 headers = {'Ocp-Apim-Subscription-Key': subscription_key}
