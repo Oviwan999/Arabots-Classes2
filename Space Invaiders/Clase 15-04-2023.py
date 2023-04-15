108d33de10ab587d0dee27d3de962f4a56cf1da0
Rodrigo R <a01183284@tec.mx>
2023-04-15 10:47:32 -0600

    subiendo varioas pruebas

--- Space Invaiders/Clase 15-04-2023.py
+++ Space Invaiders/Clase 15-04-2023.py
@@ -5,6 +5,7 @@ from pygame import mixer
 
 #Inicializamos el juego
 pygame.init()
+
 # crear pantalla
 pantalla = pygame.display.set_mode((800, 600))
 
@@ -13,3 +14,4 @@ pygame.display.set_caption("Invasión Espacial")
 icono = pygame.image.load("Space Invaiders/ovni.png")
 pygame.display.set_icon(icono)
 fondo = pygame.image.load('Space Invaiders/Fondo.jpg')
+
